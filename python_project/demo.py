4b795da2c69a40d8f58ed51ba83be2a65c563e5b
prajasunil17 <69504834+prajasunil17@users.noreply.github.com>
2025-03-15 16:44:24 +0530

    Web application demo

---
+++ python_project/demo.py
@@ -0,0 +1,13 @@
+print("Hello Python")
+
+rows =5
+for i in range(1, rows+1):
+    print("*" * i)
+
+    
+if rows == 5:
+    print("Five is greater than two!") 
+    if 5 > 2:
+        print("Five is greater than two!") 
+        
+        
